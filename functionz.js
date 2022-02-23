0f6447945342d4849e1b42fcdbe952fe8a864f96
saurabh <saurabhkedar@gmail.com>
2022-02-23 11:58:33 +0530

    updated desgin

--- functionz.js
+++ functionz.js
@@ -4,7 +4,9 @@ const amtA = document.getElementById('amount-one')
 const currencyB = document.getElementById('currency-Two')
 const amtB = document.getElementById('amount-two')
 
-const rate = document.getElementById('rate')
+const cardy = document.querySelector('.cardy')
+
+const rateX = document.getElementById('rate')
 const swap = document.getElementById('swap')
 
 currencyA.addEventListener('change', grab)
@@ -12,26 +14,35 @@ amtA.addEventListener('input', grab)
 currencyB.addEventListener('change', grab)
 amtB.addEventListener('input', grab)
 
+function read() {
+  cardy.innerText = rate.innerText
+}
+
 function grab() {
   const currencyValueA = currencyA.value
   const currencyValueB = currencyB.value
 
   fetch(
-    `https://v6.exchangerate-api.com/v6/e0ee3020537fd414c0d72852/latest/${currencyValueA}`
+    `https://v6.exchangerate-api.com/v6/5af81cb7abd6677b99760f65/latest/${currencyValueA}`
   )
     .then((res) => res.json())
     .then((data) => {
       const theRate = data.conversion_rates[currencyValueB]
-      rate.innerText = `1 ${currencyValueA}=${theRate} ${currencyValueB}`
+      rateX.innerText = `1 ${currencyValueA} = ${theRate} ${currencyValueB}`
       amtB.value = (amtA.value * theRate).toFixed(2)
-
-      swap.addEventListener('click', () => {
-        const temp = currencyA.value
-        currencyA.value = currencyB.value
-        currencyB.value = temp
-        grab()
-      })
+      read()
     })
 }
 
+swap.addEventListener('click', () => {
+  const temp = currencyA.value
+  currencyA.value = currencyB.value
+  currencyB.value = temp
+  grab()
+})
+
 grab()
+
+// if Api key fails try the one below
+//Your API Key: 9fe149352b8193ce07cbb833
+// Example Request: https://v6.exchangerate-api.com/v6/9fe149352b8193ce07cbb833/latest/USD
